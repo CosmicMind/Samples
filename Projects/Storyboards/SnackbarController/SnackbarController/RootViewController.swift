fe596b0cc6c68aa13c4758dec810fdb8f03c9784
Daniel Dahan <daniel@cosmicmind.com>
2017-03-07 18:13:33 -0600

    added Storyboard samples

---
+++ Projects/Storyboards/SnackbarController/SnackbarController/RootViewController.swift
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2015 - 2017, Daniel Dahan and CosmicMind, Inc. <http://cosmicmind.com>.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *	*	Redistributions of source code must retain the above copyright notice, this
+ *		list of conditions and the following disclaimer.
+ *
+ *	*	Redistributions in binary form must reproduce the above copyright notice,
+ *		this list of conditions and the following disclaimer in the documentation
+ *		and/or other materials provided with the distribution.
+ *
+ *	*	Neither the name of CosmicMind nor the names of its
+ *		contributors may be used to endorse or promote products derived from
+ *		this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import UIKit
+import Material
+
+class RootViewController: UIViewController {
+    fileprivate var undoButton: FlatButton!
+    
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = Color.grey.lighten5
+        
+        prepareUndoButton()
+    }
+    
+    open override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        prepareSnackbar()
+        animateSnackbar()
+        scheduleAnimation()
+    }
+}
+
+extension RootViewController {
+    fileprivate func prepareUndoButton() {
+        undoButton = FlatButton(title: "Undo", titleColor: Color.yellow.base)
+        undoButton.pulseAnimation = .backing
+        undoButton.titleLabel?.font = snackbarController?.snackbar.textLabel.font
+    }
+    
+    fileprivate func prepareSnackbar() {
+        guard let snackbar = snackbarController?.snackbar else {
+            return
+        }
+        
+        snackbar.text = "Reminder saved."
+        snackbar.rightViews = [undoButton]
+    }
+    
+    fileprivate func scheduleAnimation() {
+        Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(animateSnackbar), userInfo: nil, repeats: true)
+    }
+}
+
+extension RootViewController {
+    @objc
+    fileprivate func animateSnackbar() {
+        guard let sc = snackbarController else {
+            return
+        }
+        
+        _ = sc.animate(snackbar: .visible, delay: 1)
+        _ = sc.animate(snackbar: .hidden, delay: 4)
+    }
+}
+
